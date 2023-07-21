8426860967adf870d7633ee211e95ab5be1459f6
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-21 08:33:37 +0530

    Initial Commit

---
+++ Unit Conversion App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unit Conversion App
+//
+//  Created by Shubham on 21/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Unit Conversion App/Unit_Conversion_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_Conversion_AppApp.swift
+//  Unit Conversion App
+//
+//  Created by Shubham on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit_Conversion_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
