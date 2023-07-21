4937c1e257ec52d0b8e41628f19319e7a70eb2ff
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-21 09:13:45 +0530

    Unit converter

--- Unit Conversion App/ContentView.swift
+++ Unit Conversion App/ContentView.swift
@@ -7,15 +7,82 @@
 
 import SwiftUI
 
+enum TemperatureUnits: String {
+    case celsius = "Celsius"
+    case fahrenheit = "Fahrenheit"
+    case kelvin = "Kelvin"
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputTemperature = 0.0
+    let units = [TemperatureUnits.celsius.rawValue, TemperatureUnits.fahrenheit.rawValue, TemperatureUnits.kelvin.rawValue]
+    @State private var selectedInputUnit = "Celsius"
+    @State private var selectedOutputUnit = "Fahrenheit"
+    
+    private var outputTemperature: Double {
+        let selectedInputUnit = selectedInputUnit
+        let selectedOutputUnit = selectedOutputUnit
+        var outputTemperatureValue = 0.0
+        
+        if selectedInputUnit == TemperatureUnits.celsius.rawValue {
+            if selectedInputUnit == selectedOutputUnit {
+                outputTemperatureValue = inputTemperature
+            } else if selectedOutputUnit == TemperatureUnits.fahrenheit.rawValue {
+                outputTemperatureValue = (inputTemperature * 9/5) + 32
+            } else if selectedOutputUnit == TemperatureUnits.kelvin.rawValue {
+                outputTemperatureValue = inputTemperature + 273.15
+            }
+            
+        } else if selectedInputUnit == TemperatureUnits.fahrenheit.rawValue {
+            if selectedInputUnit == selectedOutputUnit {
+                outputTemperatureValue = inputTemperature
+            } else if selectedOutputUnit == TemperatureUnits.celsius.rawValue {
+                outputTemperatureValue = 5/9 * (inputTemperature - 32)
+            } else if selectedOutputUnit == TemperatureUnits.kelvin.rawValue {
+                outputTemperatureValue = (inputTemperature - 32) * 5/9 + 273.15
+            }
+        } else if selectedInputUnit == TemperatureUnits.kelvin.rawValue {
+            if selectedInputUnit == selectedOutputUnit {
+                outputTemperatureValue = inputTemperature
+            } else if selectedOutputUnit == TemperatureUnits.celsius.rawValue {
+                outputTemperatureValue = inputTemperature - 273.15
+            } else if selectedOutputUnit == TemperatureUnits.fahrenheit.rawValue {
+                outputTemperatureValue = (inputTemperature - 273.15) * 9/5 + 32
+            }
+            
         }
-        .padding()
+        
+        return outputTemperatureValue
+    }
+    
+    var body: some View {
+        Form {
+            // MARK: Input value
+            Section {
+                TextField("Enter Value", value: $inputTemperature, format: .number)
+                
+                Picker("Input unit", selection: $selectedInputUnit) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
+                    }
+                }
+            } header: {
+                Text("Input temperature value")
+            }
+
+            
+            // MARK: Out value
+            Section {
+                Text("\(outputTemperature, specifier: "%.2f")")
+                Picker("Output unit", selection: $selectedOutputUnit) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
+                    }
+                }
+            } header: {
+                Text("Output temperature value")
+            }
+        }
     }
 }
 
